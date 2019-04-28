623f27f39b4bffa5123de46b8ed7f2827fb0de78
NamVo5009 <envytheroy@gmail.com>
2019-04-28 16:53:46 -0400

    init setup

---
+++ README.md
@@ -0,0 +1,39 @@
+# Fandangit
+
+> This was a group project I worked on during my time at Hack Reactor. The focus was on the front-end with less emphasis on server-side and database implementation. The goal was to clone a popular website's item page using our own randomly generated data. Each group member was tasked a separate component to work on.
+> Some of the technologies utilized in this project were React, Node, Express, Mongo DB, AWS Elastic Beanstalk, Mongo Atlas and more.
+
+## Related Projects
+
+  - https://github.com/team-fry-hrr37-fec/main-video-info
+  - https://github.com/team-fry-hrr37-fec/video-player-and-carousel
+  - https://github.com/team-fry-hrr37-fec/cast-and-crew
+
+## Table of Contents
+
+1. [Usage](#Usage)
+1. [Requirements](#requirements)
+1. [Development](#development)
+
+## Usage
+
+> Some usage instructions
+
+## Requirements
+
+An `nvmrc` file is included if using [nvm](https://github.com/creationix/nvm).
+
+- Node 6.13.0
+- etc
+
+## Development
+
+### Installing Dependencies
+
+From within the root directory:
+
+```sh
+npm install -g webpack
+npm install
+```
+
---
+++ server/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const proxy = require('http-proxy-middleware');
+const cors = require('cors');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+// serve up static file
+app.use(express.static('public'));
+app.use('/*/styles.css', express.static('public/styles.css'));
+app.use('/*', express.static('public'));
+
+//
+// app.use(express.static(__dirname + '/../client/dist'));
+// app.use('/*/styles.css', express.static(__dirname + '/../client/dist/styles.css'));
+// app.use('/*', express.static(__dirname + '/../client/dist'));
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
+
+
+
+// video carousel
+const videoCarouselOptions = {
+  target: 'http://localhost:3333',
+  // changeOrigin: true
+};
+const videoCarouselProxy = proxy(videoCarouselOptions);
+app.use('/associatedVideos', videoCarouselProxy);
+
+
+const castCrewOptions = {
+  target: 'http://localhost:2002',
+  // changeOrigin: true
+};
+const castCrewProxy = proxy(castCrewOptions);
+app.use('/actors', castCrewProxy);
+
+
+const movieInfoOptions = {
+  target: 'http://localhost:2000',
+  // changeOrigin: true
+};
+const movieInfoProxy = proxy(movieInfoOptions);
+app.use('/movies', movieInfoProxy);
